b88ec458ebc9dbf92b6dd1030ed634d431b7acc1
Elias Skogevall <elias.skogevall@gmail.com>
2022-06-17 09:17:46 +0200

    Refactor eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,21 @@
-const ts = rule => `@typescript-eslint/${rule}`
+const ts = rule => `@typescript-eslint/${rule}`;
+
+const shared = {
+	indent: ["error", "tab", { SwitchCase: 1 }],
+	quotes: ["error", "single"],
+	semi: ["error", "always"],
+	"eol-last": ["error", "always"],
+	"eqeqeq": "error",
+	"linebreak-style": ["error", "unix"],
+	"no-cond-assign": "off",
+	"no-constant-condition": "warn",
+	"no-empty": "off",
+	"no-empty-function": "off",
+	"no-restricted-imports": ["error", { patterns: ["**/index"] }],
+	"no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+	"prefer-as-const": "off",
+	"quote-props": ["error", "as-needed"],
+};
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
@@ -17,23 +34,7 @@ module.exports = {
 			extends: [
 				"eslint:recommended"
 			],
-			rules: {
-				// Sort alphabetically to avoid merge conflicts
-				indent: ["error", "tab", { SwitchCase: 1 }],
-				quotes: ["error", "single"],
-				semi: ["error", "always"],
-				"eol-last": ["error", "always"],
-				"eqeqeq": "error",
-				"linebreak-style": ["error", "unix"],
-				"no-cond-assign": "off",
-				"no-constant-condition": "warn",
-				"no-empty": "off",
-				"no-empty-function": "off",
-				"no-restricted-imports": ["error", { patterns: ["**/index"] }],
-				"no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-				"prefer-as-const": "off",
-				"quote-props": ["error", "as-needed"],
-			},
+			rules: shared,
 		},
 		{
 			files: "**/*.ts",
@@ -52,26 +53,24 @@ module.exports = {
 				]
 			},
 			rules: {
-				indent: ["error", "tab", { SwitchCase: 1 }],
-				quotes: ["error", "single"],
-				"eol-last": ["error", "always"],
-				"eqeqeq": "error",
-				"linebreak-style": ["error", "unix"],
-				"no-cond-assign": "off",
-				"no-constant-condition": "warn",
-				"no-empty": "off",
-				"no-restricted-imports": ["error", { patterns: ["**/index"] }],
-				"quote-props": ["error", "as-needed"],
+				...shared,
+
+				"no-unused-vars": "off",
 				[ts("no-unused-vars")]: ["warn", { argsIgnorePattern: "^_" }],
+
+				"prefer-as-const": "off",
+				[ts("prefer-as-const")]: "off",
+
+				"semi": "off",
+				[ts("semi")]: ["error", "always"],
+
 				[ts("ban-ts-comment")]: "error",
 				[ts("no-empty-function")]: "off",
 				[ts("no-empty-interface")]: "off",
 				[ts("no-explicit-any")]: "off",
 				[ts("no-non-null-assertion")]: "off",
-				[ts("prefer-as-const")]: "off",
 				[ts("restrict-plus-operands")]: "error",
 				[ts("restrict-template-expressions")]: "error",
-				[ts("semi")]: ["error", "always"],
 				[ts("strict-boolean-expressions")]: "error",
 			},
 		}
