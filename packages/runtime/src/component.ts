27f46d77f1382218d1413ff855083e3ba495d172
Elias Skogevall <elias.skogevall@gmail.com>
2022-06-18 09:50:13 +0200

    Make component init options optional

--- packages/runtime/src/component.ts
+++ packages/runtime/src/component.ts
@@ -1,11 +1,12 @@
 import { Lifecycle } from "./lifecycle";
 
 export interface ComponentOptions {
+	[key: string]: unknown;
 	props?: Record<string, unknown>
 }
 
 export declare class Component implements Lifecycle {
-	constructor(options: ComponentOptions);
+	constructor(options?: ComponentOptions);
 	insert(target: ParentNode, anchor?: Node): void;
 	destroy(): void;
 }
