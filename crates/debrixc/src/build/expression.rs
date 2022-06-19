6e27beb8b34b5473fba7b7b505ded5bb1141eb32
Elias Skogevall <elias.skogevall@gmail.com>
2022-06-19 09:05:30 +0200

    Add function to build expression as reference from model

--- crates/debrixc/src/build/expression.rs
+++ crates/debrixc/src/build/expression.rs
@@ -2,37 +2,88 @@ use super::Build;
 use crate::{chunk::Chunk, parser::Rule};
 use pest::iterators::Pair;
 
+fn very_expression(pair: &Pair<Rule>) {
+	if pair.as_rule() != Rule::expression {
+		panic!("Node is not an expression.");
+	}
+}
+
 impl Build {
-	pub fn build_expression(&mut self, expression: Pair<Rule>) -> Chunk {
-		if expression.as_rule() != Rule::expression {
-			panic!("Node is not an expression.");
+	pub fn build_expression_as_reference(&mut self, expression: Pair<Rule>) -> Chunk {
+		very_expression(&expression);
+
+		let expr_chunk = self.build_expression(expression.clone());
+
+		let mut inner = expression.into_inner();
+		let left = inner.next().unwrap();
+		let right = inner.next();
+
+		fn is_ident(left: &Pair<Rule>, right: &Option<Pair<Rule>>) -> bool {
+			left.as_rule() == Rule::literal && right.is_none()
 		}
 
+		fn is_member(_left: &Pair<Rule>, right: &Option<Pair<Rule>>) -> bool {
+			right.is_some() && right.as_ref().unwrap().as_rule() == Rule::member_expression
+		}
+
+		if inner.peek().is_none() && is_ident(&left, &right) || is_member(&left, &right) {
+			let mut chunk = Chunk::new();
+			chunk.append("this.$reference(");
+			chunk.append_chunk(expr_chunk);
+			chunk.append(");");
+			return chunk;
+		}
+
+		expr_chunk
+	}
+
+	pub fn build_expression(&mut self, expression: Pair<Rule>) -> Chunk {
+		very_expression(&expression);
+
 		let mut chunk = Chunk::new();
-		let pair = expression.into_inner().next().unwrap();
+		let mut inner = expression.into_inner();
 
-		match pair.as_rule() {
-			Rule::binary_expression => {
-				let mut inner = pair.into_inner();
-				let operator = inner.next().unwrap();
-				let right = inner.next().unwrap();
+		while let Some(pair) = inner.next() {
+			match pair.as_rule() {
+				Rule::binary_expression => {
+					let mut inner = pair.into_inner();
+					let operator = inner.next().unwrap();
+					let right = inner.next().unwrap();
 
-				chunk.append(" ");
-				chunk.append(operator.as_str());
-				chunk.append(" ");
-				chunk.append_chunk(self.build_expression(right));
-			}
+					chunk.append(" ");
+					chunk.append(operator.as_str());
+					chunk.append(" ");
+					chunk.append_chunk(self.build_expression(right));
+				}
 
-			Rule::new_expression => {
-				let mut inner = pair.into_inner();
-				let target = inner.next().unwrap();
-				let arguments = inner.next();
+				Rule::new_expression => {
+					let mut inner = pair.into_inner();
+					let target = inner.next().unwrap();
+					let arguments = inner.next();
 
-				chunk.append("new ");
-				chunk.append_chunk(self.build_expression(target));
+					chunk.append("new ");
+					chunk.append_chunk(self.build_expression(target));
 
-				if let Some(arguments) = arguments {
-					chunk.append(" (");
+					if let Some(arguments) = arguments {
+						chunk.append(" (");
+						let mut inner = arguments.into_inner();
+						while let Some(argument) = inner.next() {
+							chunk.append_chunk(self.build_expression(argument));
+							if inner.peek().is_some() {
+								chunk.append(", ");
+							}
+						}
+						chunk.append(")");
+					}
+				}
+
+				Rule::call_expression => {
+					let mut inner = pair.into_inner();
+					let target = inner.next().unwrap();
+					let arguments = inner.next().unwrap();
+
+					chunk.append_chunk(self.build_expression(target));
+					chunk.append("(");
 					let mut inner = arguments.into_inner();
 					while let Some(argument) = inner.next() {
 						chunk.append_chunk(self.build_expression(argument));
@@ -42,78 +93,61 @@ impl Build {
 					}
 					chunk.append(")");
 				}
-			}
 
-			Rule::call_expression => {
-				let mut inner = pair.into_inner();
-				let target = inner.next().unwrap();
-				let arguments = inner.next().unwrap();
+				Rule::unary_expression => {
+					let mut inner = pair.into_inner();
+					let operator = inner.next().unwrap();
+					let target = inner.next().unwrap();
 
-				chunk.append_chunk(self.build_expression(target));
-				chunk.append("(");
-				let mut inner = arguments.into_inner();
-				while let Some(argument) = inner.next() {
-					chunk.append_chunk(self.build_expression(argument));
-					if inner.peek().is_some() {
-						chunk.append(", ");
+					chunk.append(operator.as_str());
+					chunk.append(" ");
+					chunk.append_chunk(self.build_expression(target));
+				}
+
+				Rule::expression => {
+					let mut inner = pair.into_inner();
+					let target = inner.next().unwrap();
+
+					chunk.append("(");
+					chunk.append_chunk(self.build_expression(target));
+					chunk.append(")");
+				}
+
+				Rule::member_expression => {
+					let mut inner = pair.into_inner();
+
+					while let Some(member) = inner.next() {
+						let rule = member.as_rule();
+						let target = member.into_inner().next().unwrap();
+
+						match rule {
+							Rule::member => {
+								chunk.append(".");
+								chunk.append_chunk(self.build_expression(target));
+							}
+
+							Rule::optional_memeber => {
+								chunk.append("?.");
+								chunk.append_chunk(self.build_expression(target));
+							}
+
+							Rule::computed_member => {
+								chunk.append("[");
+								chunk.append_chunk(self.build_expression(target));
+								chunk.append("]");
+							}
+
+							_ => unreachable!(),
+						}
 					}
 				}
-				chunk.append(")");
-			}
 
-			Rule::unary_expression => {
-				let mut inner = pair.into_inner();
-				let operator = inner.next().unwrap();
-				let target = inner.next().unwrap();
-
-				chunk.append(operator.as_str());
-				chunk.append(" ");
-				chunk.append_chunk(self.build_expression(target));
-			}
-
-			Rule::expression => {
-				let mut inner = pair.into_inner();
-				let target = inner.next().unwrap();
-
-				chunk.append("(");
-				chunk.append_chunk(self.build_expression(target));
-				chunk.append(")");
-			}
-
-			Rule::member_expression => {
-				let mut inner = pair.into_inner();
-
-				while let Some(member) = inner.next() {
-					let rule = member.as_rule();
-					let target = member.into_inner().next().unwrap();
-
-					match rule {
-						Rule::member => {
-							chunk.append(".");
-							chunk.append_chunk(self.build_expression(target));
-						}
-
-						Rule::optional_memeber => {
-							chunk.append("?.");
-							chunk.append_chunk(self.build_expression(target));
-						}
-
-						Rule::computed_member => {
-							chunk.append("[");
-							chunk.append_chunk(self.build_expression(target));
-							chunk.append("]");
-						}
-
-						_ => unreachable!(),
-					}
+				Rule::ident | Rule::literal => {
+					chunk.append(pair.as_str());
 				}
-			}
 
-			Rule::ident | Rule::literal => {
-				chunk.append(pair.as_str());
+				_ => unimplemented!(),
 			}
-
-			_ => unimplemented!(),
 		}
 
 		chunk
