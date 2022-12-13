9a1646bdcb17a69a014a6468d157887794be9bd9
Elias Skogevall <elias.skogevall@gmail.com>
2022-12-13 18:13:28 +0100

    refactor: remove ts-ignore comment (#44)

--- packages/debrix-compiler/src/node_worker.ts
+++ packages/debrix-compiler/src/node_worker.ts
@@ -10,14 +10,13 @@ const require = createRequire(import.meta.url);
 
 declare const NATIVE_MODULE_PATH: string;
 
-// @ts-ignore
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const compiler = require(NATIVE_MODULE_PATH) as import('debrix.node');
+const compiler = require(NATIVE_MODULE_PATH) as typeof import('debrix.node');
 
 import { parentPort } from 'node:worker_threads';
 import type { InternalBuildObject, InternalErrorObject } from './common';
 
-parentPort!.on('message', ([handle, input, target]) => {
+parentPort!.on('message', ([handle, input, target]: [number, string, number]) => {
 	let build: InternalBuildObject | undefined,
 		error: InternalErrorObject | undefined;
 
