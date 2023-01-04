b80dd251966c4c54dcbad157938bdf7fac7dbeeb
Elias Skogevall <elias.skogevall@gmail.com>
2023-01-05 00:42:56 +0100

    refactor: use prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,62 @@
 // @ts-check
 
+const ts = (rules) =>
+	Object.fromEntries(
+		Object.entries(rules).map(([key, value]) => [
+			'@typescript-eslint/' + key,
+			value,
+		])
+	);
+
+// Note! Sort eslint rules alphabetically!
+
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-	extends: '@debrix',
 	env: {
 		es2021: true,
 		node: true,
 		browser: true,
 	},
 	parserOptions: {
-		project: './packages/*/tsconfig.json'
-	}
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: './packages/*/tsconfig.json',
+	},
+	extends: ['eslint:recommended', 'prettier'],
+	rules: {
+		eqeqeq: 'error',
+		'comma-dangle': ['error', 'only-multiline'],
+		'no-cond-assign': 'off',
+		'no-constant-condition': 'warn',
+		'no-empty': 'off',
+		'no-empty-function': 'off',
+		'no-restricted-imports': ['error', { patterns: ['**/index'] }],
+		'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+		'prefer-as-const': 'off',
+	},
+	overrides: [
+		{
+			files: ['*.ts', '*.tsx', '*.mts', '*.cts'],
+			extends: [
+				'plugin:@typescript-eslint/recommended',
+				'plugin:@typescript-eslint/recommended-requiring-type-checking',
+				'plugin:@typescript-eslint/strict',
+			],
+			rules: {
+				...ts({
+					'ban-ts-comment': [
+						'error',
+						{ 'ts-expect-error': 'allow-with-description' },
+					],
+					'no-empty-function': 'off',
+					'no-empty-interface': 'off',
+					'no-explicit-any': 'off',
+					'no-invalid-void-type': ['warn', { allowAsThisParameter: true }],
+					'no-non-null-assertion': 'off',
+					'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+					'strict-boolean-expressions': 'error',
+				}),
+			},
+		},
+	],
 };
